1377d60ab5f4999fa3cdaa32901b115099967cc7
dhansharma <dhan@jigsaw.co>
2023-03-20 19:34:13 +0000

    Remove files under build folder.

--- build/spotless/spotlessKotlin/app/src/main/java/com/scale/whiteshadow/ui/components/PokemonStatsInfo.kt
+++ build/spotless/spotlessKotlin/app/src/main/java/com/scale/whiteshadow/ui/components/PokemonStatsInfo.kt
@@ -27,14 +27,22 @@ fun PokemonStatsInfo(modifier: Modifier = Modifier, content: PokemonInfo) {
 
 @Composable
 fun StatsItem(statName: String, statValue: Int) {
-    Row(modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp), verticalAlignment = Alignment.CenterVertically) {
-        Text(text = statName.uppercase())
-        Divider(
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .width(1.dp)
+    Column(modifier = Modifier.padding(vertical = 10.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = statName.uppercase())
+            Divider(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .width(1.dp)
+            )
+            Text(modifier = Modifier.padding(start = 8.dp), text = statValue.toString())
+        }
+        LinearProgressIndicator(
+            modifier = Modifier.padding(top = 4.dp)
+                .fillMaxWidth(),
+            progress = statValue.toFloat() / 255
         )
-        Text(modifier = Modifier.padding(start = 8.dp), text = statValue.toString())
-        LinearProgressIndicator(modifier = Modifier.fillMaxWidth().padding(start = 8.dp), progress = statValue.toFloat() / 255)
     }
 }
